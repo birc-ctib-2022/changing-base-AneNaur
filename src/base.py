aa84958698161293ab74d3bd55c5bba6dbd23165
AneNaur <202008164@post.au.dk>
2022-09-14 13:02:08 +0200

    base

--- src/base.py
+++ src/base.py
@@ -13,20 +13,32 @@ digits[14] = 'E'
 digits[15] = 'F'
 
 
-def change_to_base(n: int, b: int) -> str:
+def change_to_base(n,b): #(n: int, b: int) -> str:
     """
     Return `n` in base `b`.
 
     The base `b` must be in the range 2 to 16.
 
     >>> change_to_base(1, 2)
-    '1'
+    '1*2^0 = 1'
     >>> change_to_base(31, 2)
-    '11111'
+    '31//2=15 (rest 1) -> 15//2=7 (rest 1) -> 7//2=3 (rest 1) -> 3//2=1 (rest 1) -> 1//2=0 (rest 1) => 11111'
     >>> change_to_base(31, 8)
-    '37'
+    '31//8=3 (rest 7) -> 3//8=0 (rest 3) => 37'
     >>> change_to_base(31, 16)
-    '1F'
+    '31//16=1 (rest 15=F) -> 1//16=0 (rest 1) => 1F'
     """
     assert 2 <= b <= 16
-    return ''  # FIXME: return n in the right base
+    liste=[]
+    x=n
+    while x >= b:
+        liste.append(digits[x%b])
+        x=x//b
+    else:
+        liste.append(digits[x%b])
+    return "".join(liste[::-1])  # FIXME: return n in the right base
+
+#print(change_to_base(1,2))
+#print(change_to_base(31,2))
+#print(change_to_base(31,8))
+#print(change_to_base(31,16))
